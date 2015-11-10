69ece639ad8c0393813ac83388568cd73287590b
Marlo Major <MarloMajor@yahoo.com>
2015-11-09 18:54:05 -0700

    Change Root in Routes
    - Set root in routes to cities index

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
+  root to: "cities#index"
   resources :cities, only: [:index, :show] do
-    resources :categories, only: [:show] 
+    resources :categories, only: [:show]
   end
 end
