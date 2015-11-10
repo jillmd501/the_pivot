083b8ef1f5b81ffa526483078f0174375b21aa6f
Pat Wey <patrickwey@gmail.com>
2015-11-10 10:34:01 -0700

    Add seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,32 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+city = City.create(name: 'Vail', image_path: 'image.jpg')
+city.categories.create(name: 'Entertainment')
+city.categories.create(name: 'Food & Drink')
+city.categories.first.options.create(name: 'Snowshoeing',
+                                     description: 'So fun!',
+                                     price: 75)
+city.categories.first.options.create(name: 'Skiing',
+                                     description: 'Love it!',
+                                     price: 100)
+city.categories.last.options.create(name: 'Brewery Tour',
+                                    description: 'Mmm, Beer!',
+                                    price: 20)
+city.categories.last.options.create(name: 'Traveling Dinner',
+                                    description: 'Restaurant hopping!',
+                                    price: 40)
+
+
+city = City.create(name: 'Golden', image_path: 'image.jpg')
+city.categories.create(name: 'Entertainment')
+city.categories.create(name: 'Food & Drink')
+city.categories.first.options.create(name: 'Mountain Biking',
+                                    description: 'Woohoo!',
+                                    price: 30)
+city.categories.first.options.create(name: 'Trail Running',
+                                    description: 'Yessss!',
+                                    price: 20)
+city.categories.last.options.create(name: 'Coors Brewery Tour',
+                                    description: 'Mmm, Beer!',
+                                    price: 20)
+city.categories.last.options.create(name: 'Saloon Show',
+                                    description: 'Wild West Action!',
+                                    price: 40)
