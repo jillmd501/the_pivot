7e7060c386d6443ba563418e281c4e42e7de7906
Pat Wey <patrickwey@gmail.com>
2015-11-10 10:34:31 -0700

    Style views
    * Add navbar
    * Add cards to cities index

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,8 +10,8 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
-require jquery
-require materialize-sprockets
+//= require jquery
+//= require materialize-sprockets
 //= require jquery_ujs
 //= require turbolinks
 //= require_tree .
