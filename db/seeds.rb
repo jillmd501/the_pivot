64fcc110b36f96dbe0927e687e9ef810f61b1615
Marlo Major <MarloMajor@yahoo.com>
2015-11-10 12:53:56 -0700

    Fix hound errors
    -Correct hash spacing
    -remove unnecessary quotes

--- db/seeds.rb
+++ db/seeds.rb
@@ -15,17 +15,16 @@ city.categories.last.options.create(name: "Traveling Dinner",
                                     description: "Restaurant hopping!",
                                     price: 40)
 
-
 city = City.create(name: "Golden",
                    image_path: "https://images.unsplash.com/photo-1423450822265-fcd97e52ecb5?ixlib=rb-0.3.5&q=80&fm=jpg&s=b4e113de22023f00f2d8aa1b3de11492")
 city.categories.create(name: "Entertainment")
 city.categories.create(name: "Food & Drink")
 city.categories.first.options.create(name: "Mountain Biking",
-                                    description: "Woohoo!",
-                                    price: 30)
+                                     description: "Woohoo!",
+                                     price: 30)
 city.categories.first.options.create(name: "Trail Running",
-                                    description: "Yessss!",
-                                    price: 20)
+                                     description: "Yessss!",
+                                     price: 20)
 city.categories.last.options.create(name: "Coors Brewery Tour",
                                     description: "Mmm, Beer!",
                                     price: 20)
