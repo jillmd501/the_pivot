e9d4fc15d969a531d3207a1a3b9ecdee076cd3f4
Marlo Major <MarloMajor@yahoo.com>
2015-11-10 19:14:24 -0700

    fix hound
    - remove trailing white space
    - double quotes instead of single

--- test/integration/visitor_can_add_trip_to_cart_test.rb
+++ test/integration/visitor_can_add_trip_to_cart_test.rb
@@ -1,4 +1,4 @@
-require 'test_helper'
+require "test_helper"
 
 class VisitorCanAddTripToCartTest < ActionDispatch::IntegrationTest
   test "visitor can add trip to cart" do
--- test/models/option_test.rb
+++ test/models/option_test.rb
@@ -1,5 +1,4 @@
 require 'test_helper'
 
 class OptionTest < ActiveSupport::TestCase
-  
 end
