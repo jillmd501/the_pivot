4eb30c591f7724e870699ae86d28b18d3b970a50
Marlo Major <MarloMajor@yahoo.com>
2015-11-14 14:21:17 -0700

    fix admin hound errors
    - single to double quotes
    - removed unnecessary assignments

--- test/integration/admin_dashboard_direct_login_test.rb
+++ test/integration/admin_dashboard_direct_login_test.rb
@@ -2,10 +2,10 @@ require 'test_helper'
 
 class AdminDashboardDirectLoginTest < ActionDispatch::IntegrationTest
   test 'admin can login to admin dashboard' do
-  admin_creates_account
-  admin_logs_in
+    admin_creates_account
+    admin_logs_in
 
-  assert_equal admin_dashboard_index_path, current_path
-  assert page.has_content? "Admin Dashboard"
+    assert_equal admin_dashboard_index_path, current_path
+    assert page.has_content? "Admin Dashboard"
   end
 end
