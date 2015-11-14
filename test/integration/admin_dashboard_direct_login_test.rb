c2fd3d94df530eccdd7540789ca3531e9d96a89b
Marlo Major <MarloMajor@yahoo.com>
2015-11-14 14:17:22 -0700

    Admin can login to dashboard
    - Admin can login directly to Admin Dashboard

---
+++ test/integration/admin_dashboard_direct_login_test.rb
@@ -0,0 +1,11 @@
+require 'test_helper'
+
+class AdminDashboardDirectLoginTest < ActionDispatch::IntegrationTest
+  test 'admin can login to admin dashboard' do
+  admin_creates_account
+  admin_logs_in
+
+  assert_equal admin_dashboard_index_path, current_path
+  assert page.has_content? "Admin Dashboard"
+  end
+end
