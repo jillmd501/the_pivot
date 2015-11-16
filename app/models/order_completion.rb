54ac546b9b60502af3a4b085c8b68cb6c2da7f5e
Jill Donohue <jilldonohue@Jills-MacBook-3.local>
2015-11-16 12:47:37 -0700

    Set default status to paid

--- app/models/order_completion.rb
+++ app/models/order_completion.rb
@@ -1,7 +1,8 @@
 class OrderCompletion
   def self.create(user, cart)
     order = user.orders.new(total: 0,
-                            status_timestamp: formatted_time)
+                            status_timestamp: formatted_time,
+                            status: "Paid")
     cart.trips.each do |cart_trip|
       order.order_trips.new(trip_id: cart_trip.id,
                             subtotal: cart_trip.subtotal,
