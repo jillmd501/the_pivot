bce0e4c4aeaa9df75ef97cf18b725ddd3caa0855
Pat Wey <patrickwey@gmail.com>
2015-11-18 12:08:39 -0700

    Add Return to Trips button to trip show page

--- test/integration/visitor_can_see_trips_in_a_city_test.rb
+++ test/integration/visitor_can_see_trips_in_a_city_test.rb
@@ -1,7 +1,7 @@
 require "test_helper"
 
 class VisitorCanSeeTripsInACityTest < ActionDispatch::IntegrationTest
-  test 'visitor can see trips within a city' do
+  test "visitor can see trips within a city" do
     create_city
     trip = create_trip(@city)
     visit cities_path
@@ -16,5 +16,19 @@ class VisitorCanSeeTripsInACityTest < ActionDispatch::IntegrationTest
     assert page.has_content?("Outdoor fun")
     assert page.has_content?("biking")
     assert page.has_content?("Price: $100")
+  end
+
+  test "visitor can see information about a trip" do
+    create_city
+    trip = create_trip(@city)
+    visit cities_path
+    click_link "Vail"
+
+    within ".outdoor-fun" do
+      click_link "Learn More"
+    end
+
+    assert page.has_content? "biking"
+    assert page.has_link? "Return to Trips"
   end
 end
