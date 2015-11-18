f0a3e382872ed8b52a7dede4f76b210da62ef9ee
Pat Wey <patrickwey@gmail.com>
2015-11-17 18:13:29 -0700

    Users can be created with all attributes
    * Accept new attributes in strong params
    * Add fields to new user form

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -22,6 +22,7 @@ class UsersController < ApplicationController
   private
 
   def user_params
-    params.require(:user).permit(:username, :password)
+    params.require(:user).permit(:username, :password, :address, :city,
+                                 :state, :zip, :first_name, :last_name)
   end
 end
