7c9701f6b69d8eccf1735d719fb21365e5c97d76
Jerrod Junker <unsafepond@gmail.com>
2015-11-30 17:33:43 -0700

    user model tests
    - create four passing test on user validations
    - still need to implement relationships
    - add database cleaner and simplecov

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151130230232) do
+ActiveRecord::Schema.define(version: 20151201000433) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -50,6 +50,16 @@ ActiveRecord::Schema.define(version: 20151130230232) do
     t.datetime "updated_at",  null: false
   end
 
+  create_table "user_businesses", force: :cascade do |t|
+    t.integer  "user_id"
+    t.integer  "business_id"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+  end
+
+  add_index "user_businesses", ["business_id"], name: "index_user_businesses_on_business_id", using: :btree
+  add_index "user_businesses", ["user_id"], name: "index_user_businesses_on_user_id", using: :btree
+
   create_table "users", force: :cascade do |t|
     t.string   "username"
     t.string   "password_digest"
@@ -58,4 +68,6 @@ ActiveRecord::Schema.define(version: 20151130230232) do
     t.datetime "updated_at",      null: false
   end
 
+  add_foreign_key "user_businesses", "businesses"
+  add_foreign_key "user_businesses", "users"
 end
---
+++ test/models/user_business_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class UserBusinessTest < ActiveSupport::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+end
