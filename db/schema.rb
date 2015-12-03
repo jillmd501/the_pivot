ed8e6facfd7d9553adeb7912ebdd6e3c5c74d18c
Jill Donohue <jilldonohue@Jills-MacBook-4.local>
2015-12-03 14:15:02 -0700

    Remove addresses table

--- db/schema.rb
+++ db/schema.rb
@@ -11,24 +11,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151203171208) do
+ActiveRecord::Schema.define(version: 20151203211130) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
-
-  create_table "addresses", force: :cascade do |t|
-    t.string   "address_line_1"
-    t.string   "address_line_2"
-    t.string   "city"
-    t.string   "state"
-    t.integer  "zipcode"
-    t.string   "country"
-    t.integer  "user_id"
-    t.datetime "created_at",     null: false
-    t.datetime "updated_at",     null: false
-  end
-
-  add_index "addresses", ["user_id"], name: "index_addresses_on_user_id", using: :btree
 
   create_table "businesses", force: :cascade do |t|
     t.string   "name"
