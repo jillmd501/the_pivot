b35bfa4ebaf18e38a707bad81f1f47c9bc12d0d1
rossedfort <rossedfort@gmail.com>
2015-12-03 21:19:18 -0700

    add other 11 photos
    - fix styling on photo index page

--- db/seeds.rb
+++ db/seeds.rb
@@ -50,7 +50,18 @@ class Seed
   end
 
   def generate_photos
-    Photo.create!(name: "Couple on bench", price: 10, description: "A nice shot of a man and women sitting on a bench accross from a lake", business_id: 1, image_file_name: "photo_1", image_content_type: "image/jpg", image_file_size: 8.11, image_updated_at: Time.now)
+    Photo.create!(name: "Couple on Bench", price: 10, description: "A nice shot of a man and women sitting on a bench accross from a lake", business_id: 1, image_file_name: "photo_1", image_content_type: "image/jpg", image_file_size: 2, image_updated_at: Time.now)
+    Photo.create!(name: "Couple on Bridge", price: 8, description: "A couple on a bridge at sunset", business_id: 1, image_file_name: "photo_2", image_content_type: "image/jpg", image_file_size: 2, image_updated_at: Time.now)
+    Photo.create!(name: "Personwearing Beanie", price: 9, description: "An ambiguous form wearing a warm coat and beainie on a foggy winter day", business_id: 1, image_file_name: "photo_3", image_content_type: "image/jpg", image_file_size: 2, image_updated_at: Time.now)
+    Photo.create!(name: "Red-haired Woman Reacts", price: 12, description: "A red-haired woman, possibly in a business meeting, reacts joyously", business_id: 1, image_file_name: "photo_4", image_content_type: "image/jpg", image_file_size: 2, image_updated_at: Time.now)
+    Photo.create!(name: "Eyes", price: 10, description: "A beautiful black & white photo of a man peering eerily over his computer screen", business_id: 1, image_file_name: "photo_5", image_content_type: "image/jpg", image_file_size: 2, image_updated_at: Time.now)
+    Photo.create!(name: "Tunnel Vision", price: 8, description: "A trio stand at the end of a tunnel, silhouetted by the light in the background", business_id: 1, image_file_name: "photo_6", image_content_type: "image/jpg", image_file_size: 2, image_updated_at: Time.now)
+    Photo.create!(name: "Man on the Beach", price: 7, description: "A man stands near the shore at sunset", business_id: 1, image_file_name: "photo_7", image_content_type: "image/jpg", image_file_size: 2, image_updated_at: Time.now)
+    Photo.create!(name: "Fall Day", price: 6, description: "A man stands in the forest with the sun shining through the fall foliage", business_id: 1, image_file_name: "photo_8", image_content_type: "image/jpg", image_file_size: 2, image_updated_at: Time.now)
+    Photo.create!(name: "Woman at the Beach", price: 9, description: "A dark haired woman stands on the beach at dusk", business_id: 1, image_file_name: "photo_9", image_content_type: "image/jpg", image_file_size: 2, image_updated_at: Time.now)
+    Photo.create!(name: "Train Station", price: 9, description: "A woman looks away as a the train whizzes by", business_id: 1, image_file_name: "photo_10", image_content_type: "image/jpg", image_file_size: 2, image_updated_at: Time.now)
+    Photo.create!(name: "Hooded Figure", price: 8, description: "Somebody in a jacket standing in the forest", business_id: 1, image_file_name: "photo_11", image_content_type: "image/jpg", image_file_size: 2, image_updated_at: Time.now)
+    Photo.create!(name: "Yellow Hat", price: 10, description: "A man on a bench looks over the bay in winter", business_id: 1, image_file_name: "photo_12", image_content_type: "image/jpg", image_file_size: 2, image_updated_at: Time.now)
   end
 
   private
