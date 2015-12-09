2ffb64f9b5bf84657bfa1b5080bab948233275b4
Jerrod Junker <unsafepond@gmail.com>
2015-12-09 15:40:48 -0700

    removed un-needed methods in app helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,15 +1,15 @@
 module ApplicationHelper
-  def platform_admin?
-    current_user && current_user.platform_admin?
-  end
+  # def platform_admin?
+  #   current_user && current_user.platform_admin?
+  # end
 
-  def business_admin?
-    current_user && current_user.business_admin?
-  end
+  # def business_admin?
+  #   current_user && current_user.business_admin?
+  # end
 
-  def registered_user?
-    current_user && current_user.registered_user?
-  end
+  # def registered_user?
+  #   current_user && current_user.registered_user?
+  # end
 
   # def registered_user?
   #   if User.find(current_user.id)
